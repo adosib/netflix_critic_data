0e53106873b42b6a356aa22095b0d75a597a22cb
adosib <adosib1@gmail.com>
2025-01-12 16:08:41 -0600

    FileHandler append mode

--- scripts/database_setup/populate_availability.py
+++ scripts/database_setup/populate_availability.py
@@ -30,7 +30,7 @@ COUNTRY_CODE = "US"
 
 log_file = LOG_DIR / f'{datetime.now().strftime('%Y%m%d%H%M%S')}.log'
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler(log_file)
+file_handler = logging.FileHandler(log_file, mode="a+")
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
